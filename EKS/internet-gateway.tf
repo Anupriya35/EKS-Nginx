dd278ba3cff15da2708b41eaf82817a6bf0e5bf1
Anupriya35 <84177858+Anupriya35@users.noreply.github.com>
2021-08-05 09:40:09 +0530

    Add files via upload

--- EKS/internet-gateway.tf
+++ EKS/internet-gateway.tf
@@ -1,8 +1,6 @@
 resource "aws_internet_gateway" "main" {
-  # The VPC ID to create in.
   vpc_id = aws_vpc.main.id
-  # A map of tags to assign to the resource.
-  tags = {
+   tags = {
     Name = "main"
   }
 }
--- EKS/vpc.tf
+++ EKS/vpc.tf
@@ -1,19 +1,19 @@
 resource "aws_vpc" "main" {
-  # CIDR block for the VPC.
+ 
   cidr_block       = "192.168.0.0/16"
-  instance_tenancy = "default"
+  #instance_tenancy = "default"
 
   # Required for EKS. Enable DNS support /hostnames in the VPC.
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  # disable ClassicLink for the VPC.
+ 
   enable_classiclink = false
-  # Enable/disable ClassicLink DNS Support for the VPC.
+
   enable_classiclink_dns_support = false
-  # Requests an Amazon-provided IPv6 CIDR block with a /56 prefix length for the VPC.
+  
   assign_generated_ipv6_cidr_block = false
-  # A map of tags to assign to the resource.
+  
   tags = {
     Name = "main"
   }
@@ -21,6 +21,5 @@ resource "aws_vpc" "main" {
 output "vpc_id" {
   value       = aws_vpc.main.id
   description = "VPC id."
-  # Setting an output value as sensitive prevents Terraform from showing its value in plan and apply.
-  sensitive = false
+   sensitive = false
 }
